9e1bb5233e475c7e0c25366e5e09e9509e7a81f9
vmontanaro <veronica.montanaro@tiscali.it>
2024-01-31 13:46:19 +0100

    Added test and normalization constant

--- include/heffte_fft3d.h
+++ include/heffte_fft3d.h
@@ -641,7 +641,8 @@ private:
             }else if (std::is_same<backend_tag, backend::fftw_sin1>::value) {
                 scale_factor = 1.0 / (8.0 * (plan.fft_sizes[0] + 1) * (plan.fft_sizes[1] + 1) * (plan.fft_sizes[2] + 1));
             }else if(std::is_same<backend_tag, backend::cufft_cos1>::value or
-                     std::is_same<backend_tag, backend::rocfft_cos1>::value) {
+                     std::is_same<backend_tag, backend::rocfft_cos1>::value or
+                     std::is_same<backend_tag, backend::mkl_cos1>::value) {
                 scale_factor = 1.0 / (64.0 * (plan.fft_sizes[0] - 1) * (plan.fft_sizes[1] - 1) * (plan.fft_sizes[2] - 1));
             }else{
                 scale_factor /= 64.0;
